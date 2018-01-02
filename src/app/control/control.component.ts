9aff6c212e4e3dcc8b3b3ccf485e0de28cf38cb0
liran-cohen <lirancsoft@gmail.com>
2018-01-02 14:42:32 +0200

    Control Panel: 1. Zoom in & out 2. Navigate forward & backward 3. Toggle scrolling on/off

---
+++ src/app/control/control.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+declare var getTimeline: any;
+declare var getOptions: any;
+
+@Component({
+  selector: 'app-control',
+  templateUrl: './control.component.html',
+  styleUrls: ['./control.component.css']
+})
+export class ControlComponent implements OnInit {
+
+  timeline = null;
+  options = null;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.timeline = getTimeline();
+    this.options = getOptions();
+    console.log(this.timeline);
+    console.log(this.options);
+  }
+
+  fitAllItems() {
+    this.timeline.fit();
+  }
+
+  zoomIn() {
+    this.timeline.zoomIn(0.5);
+  }
+
+  zoomOut() {
+    this.timeline.zoomOut(0.5);
+  }
+
+  move (percentage) {
+    const range = this.timeline.getWindow();
+    const interval = range.end - range.start;
+
+    this.timeline.setWindow({
+      start: range.start.valueOf() - interval * percentage,
+      end:   range.end.valueOf()   - interval * percentage
+    });
+  }
+
+
+  moveRight() {
+    this.move(-0.5);
+  }
+
+  moveLeft() {
+    this.move(0.5);
+  }
+
+  toggleScrollingMode() {
+    if (this.options.zoomable) {
+      this.options.zoomable = false;
+    } else {
+      this.options.zoomable = true;
+    }
+    this.timeline.setOptions(this.options);
+  }
+}
