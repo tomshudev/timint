d82292cd39bf69bb46eab1c7e05814c68f83c1a6
Tom Shushan <tomshudev@gmail.com>
2018-01-07 20:20:41 +0200

    Adding the context menu to the tracks table

--- src/app/tracks/tracks.component.ts
+++ src/app/tracks/tracks.component.ts
@@ -15,6 +15,24 @@ export class TracksComponent implements OnInit {
   constructor(private tracksService: TracksService) {
     tracksService.getTracks(tracks => {
       this.tracks = tracks;
+
+      $(function() {
+        $.contextMenu({
+          selector: '.track-row',
+          animation: {duration: 500, show: 'slideDown'},
+          callback: function(key, options) {
+            console.log('The track ' + (this.children()[0].innerText) + ' will be removed');
+          },
+          items: {
+            "edit": {name: 'Edit', icon: 'edit'},
+            "delete": {name: 'Delete', icon: 'delete'},
+            "sep1": "---------",
+            "quit": {name: "Quit", icon: function(){
+                return 'context-menu-icon context-menu-icon-quit';
+            }}
+          }
+        })
+      })
     });
   }
 
